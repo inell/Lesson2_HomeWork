8eb44319528fff3fbaed22190630b01acaeeda6c
PetroKov <petrokov@mac-petrokov.local>
2016-04-14 18:05:35 +0300

    Initial Commit

---
+++ Lesson2_HomeWork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson2_HomeWork
+//
+//  Created by PetroKov on 14.04.16.
+//  Copyright © 2016 PetroKov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
